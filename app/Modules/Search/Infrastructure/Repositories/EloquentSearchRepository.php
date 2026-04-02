413a6b9f64c5dd0da4ea12fc023077d40e2aa261
Mohammed Awda <awdadm4@gmail.com>
2026-04-02 16:43:43 +0200

    optimize search api

--- app/Modules/Search/Infrastructure/Repositories/EloquentSearchRepository.php
+++ app/Modules/Search/Infrastructure/Repositories/EloquentSearchRepository.php
@@ -17,15 +17,17 @@ class EloquentSearchRepository implements SearchRepositoryInterface
 
     public function getAvailableHotels(array $filters): Collection
     {
-        $city = $filters['city'];
-        $checkIn = $filters['check_in'];
-        $checkOut = $filters['check_out'];
+        $city        = $filters['city'];
+        $checkIn     = $filters['check_in'];
+        $checkOut    = $filters['check_out'];
         $adultsCount = $filters['adults_count'];
 
         // Use tableFilter to handle city and status filtering
         $hotels = Cache::remember("search_hotels_{$city}", 3600, function () {
-            return Hotel::with(['roomTypes' => function ($query) {
+            return Hotel::select(['id'])
+                ->with(['roomTypes' => function ($query) {
                     $query->where('status', 'active');
+                    $query->select(['id', 'hotel_id', 'max_occupancy', 'base_price', 'available_rooms']);
                 }])
                 ->tableFilter(request())
                 ->get();
